23689129851192d86e0128a44e244d910a454d61
German Gonzalez <german.gonzalez@etermax.com>
2022-09-28 14:33:33 -0300

    Support Vast validation for single quote xml

--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/utils/helpers/Utils.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/utils/helpers/Utils.java
@@ -62,7 +62,7 @@ import static org.prebid.mobile.PrebidMobile.AUTO_REFRESH_DELAY_MIN;
 
 public final class Utils {
     private static final String TAG = Utils.class.getSimpleName();
-    private static final String VAST_REGEX = "<VAST\\s.*version=\".*\"(\\s.*|)?>";
+    private static final String VAST_REGEX = "<VAST[^<]+(?>version=){1}(?:'|\")\\S+(?:'|\").*?>";
 
     public static float DENSITY;
 
